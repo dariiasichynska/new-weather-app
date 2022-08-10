3896cfdc305a93738ce8e49cd1c30982ea018667
dakasun <108403405+dariiasichynska@users.noreply.github.com>
2022-08-10 12:04:06 +0200

    add background change & dateTime

--- src/app.js
+++ src/app.js
@@ -1,1 +1,56 @@
 alert("Hello world");
+let now = new Date();
+let dateTime = document.querySelector("#dateTime");
+let date = now.getDate();
+let hour = now.getHours();
+hour = hour > 9 ? hour : "0" + hour;
+let minutes = now.getMinutes();
+minutes = minutes > 9 ? minutes : "0" + minutes;
+
+let weekDay = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let dayOfTheWeek = weekDay[now.getDay()];
+
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let month = months[now.getMonth()];
+dateTime.innerHTML = ` ${hour}:${minutes} - ${date} of ${month}, ${dayOfTheWeek}`;
+
+switch (true) {
+  case 10 <= hour && hour < 18:
+    document.getElementById("weather-app").style.backgroundImage =
+      "url(day.jpg)";
+    break;
+  case 5 <= hour && hour < 10:
+    document.getElementById("weather-app").style.backgroundImage =
+      "url(sunrise.jpg)";
+    break;
+  case 18 <= hour && hour < 22:
+    document.getElementById("weather-app").style.backgroundImage =
+      "url(sunset.jpg)";
+    break;
+  case (0 <= hour && hour < 5) || 22 <= hour:
+    document.getElementById("weather-app").style.backgroundImage =
+      "url(night.jpg)";
+    break;
+}
+console.log(hour);
