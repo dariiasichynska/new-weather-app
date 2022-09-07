81a84f97e7ed5398b68563db75bf7e131eca8bb5
dakasun <108403405+dariiasichynska@users.noreply.github.com>
2022-09-07 14:31:45 +0200

    relative icon setted

--- src/app.js
+++ src/app.js
@@ -138,6 +138,60 @@ function formatWind(direction) {
   }
 }
 
+// function formatIcon(ID) {
+//   switch (true) {
+//     case (ID = `01d`):
+//       return (
+//         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64">
+//           <defs>
+//             <linearGradient
+//               id="a"
+//               x1="26.75"
+//               x2="37.25"
+//               y1="22.91"
+//               y2="41.09"
+//               gradientUnits="userSpaceOnUse"
+//             >
+//               <stop offset="0" stop-color="#fbbf24" />
+//               <stop offset=".45" stop-color="#fbbf24" />
+//               <stop offset="1" stop-color="#f59e0b" />
+//             </linearGradient>
+//           </defs>
+//           <circle
+//             cx="32"
+//             cy="32"
+//             r="10.5"
+//             fill="url(#a)"
+//             stroke="#f8af18"
+//             stroke-miterlimit="10"
+//             stroke-width=".5"
+//           />
+//           <path
+//             fill="none"
+//             stroke="#fbbf24"
+//             stroke-linecap="round"
+//             stroke-miterlimit="10"
+//             stroke-width="3"
+//             d="M32 15.71V9.5m0 45v-6.21m11.52-27.81l4.39-4.39M16.09 47.91l4.39-4.39m0-23l-4.39-4.39m31.82 31.78l-4.39-4.39M15.71 32H9.5m45 0h-6.21"
+//           >
+//             <animateTransform
+//               attributeName="transform"
+//               dur="45s"
+//               repeatCount="indefinite"
+//               type="rotate"
+//               values="0 32 32; 360 32 32"
+//             />
+//           </path>
+//         </svg>
+//       );
+//       break;
+
+//     default:
+//       `Sorry, the weather is not defined`;
+//       break;
+//   }
+// }
+
 function displayApiResults(response) {
   console.log(response.data.main.temp);
   let temperatureElement = document.querySelector("#temperature");
@@ -148,11 +202,18 @@ function displayApiResults(response) {
   conditionElement.innerHTML = response.data.weather[0].main;
   let humidityElement = document.querySelector("#humidity");
   humidityElement.innerHTML = response.data.main.humidity;
+
   let windElement = document.querySelector("#wind-power");
-  windElement.innerHTML = Math.round(response.data.wind.speed);
-  let windDirectionElement = document.querySelector("#wind-direction");
-  windDirectionElement.innerHTML = formatWind(response.data.wind.deg);
+  let windSpeed = Math.round(response.data.wind.speed);
+  windElement.innerHTML = windSpeed;
+  if (windSpeed > 0) {
+    let windDirectionElement = document.querySelector("#wind-direction");
+    windDirectionElement.innerHTML = formatWind(response.data.wind.deg);
+  }
+
+  let iconType = response.data.weather[0].icon;
   let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute("src", `openweathermap/${iconType}.svg`);
 
   // let dateElement = document.querySelector("#dateTime");
   // dateElement.innerHTML = formatDate(response.data.dt * 1000); - мне не нравится этот способ потому что
@@ -161,7 +222,7 @@ function displayApiResults(response) {
 }
 
 let apiKey = "ebfc1f6824f703866321e99d5ec95eb7";
-let cityName = "Kyiv";
+let cityName = "Sydney";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
 console.log(apiUrl);
 
