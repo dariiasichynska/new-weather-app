1f453fff9af60cbfd495caee1d9053662f0fc55e
dakasun <108403405+dariiasichynska@users.noreply.github.com>
2022-09-07 23:13:24 +0200

    comments added

--- src/app.js
+++ src/app.js
@@ -37,12 +37,16 @@ dateTime.innerHTML = ` ${dayOfTheWeek}, ${month} ${date}, ${hour}:${minutes} `;
 
 // function formatDate(timestamp) {
 //   let date = new Date(timestamp);
-//   let dayOfMonth = date.getDay();
+//   console.log(date);
+//   let dayOfMonth = date.getDate();
+//   console.log(dayOfMonth);
 //   let hours = date.getHours();
+//   console.log(hours);
 //   if (hours < 10) {
 //     hours = `0${hours}`;
 //   }
 //   let minutes = date.getMinutes();
+//   console.log(minutes);
 //   if (minutes < 10) {
 //     minutes = `0${minutes}`;
 //   }
@@ -138,60 +142,6 @@ function formatWind(direction) {
   }
 }
 
-// function formatIcon(ID) {
-//   switch (true) {
-//     case (ID = `01d`):
-//       return (
-//         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64">
-//           <defs>
-//             <linearGradient
-//               id="a"
-//               x1="26.75"
-//               x2="37.25"
-//               y1="22.91"
-//               y2="41.09"
-//               gradientUnits="userSpaceOnUse"
-//             >
-//               <stop offset="0" stop-color="#fbbf24" />
-//               <stop offset=".45" stop-color="#fbbf24" />
-//               <stop offset="1" stop-color="#f59e0b" />
-//             </linearGradient>
-//           </defs>
-//           <circle
-//             cx="32"
-//             cy="32"
-//             r="10.5"
-//             fill="url(#a)"
-//             stroke="#f8af18"
-//             stroke-miterlimit="10"
-//             stroke-width=".5"
-//           />
-//           <path
-//             fill="none"
-//             stroke="#fbbf24"
-//             stroke-linecap="round"
-//             stroke-miterlimit="10"
-//             stroke-width="3"
-//             d="M32 15.71V9.5m0 45v-6.21m11.52-27.81l4.39-4.39M16.09 47.91l4.39-4.39m0-23l-4.39-4.39m31.82 31.78l-4.39-4.39M15.71 32H9.5m45 0h-6.21"
-//           >
-//             <animateTransform
-//               attributeName="transform"
-//               dur="45s"
-//               repeatCount="indefinite"
-//               type="rotate"
-//               values="0 32 32; 360 32 32"
-//             />
-//           </path>
-//         </svg>
-//       );
-//       break;
-
-//     default:
-//       `Sorry, the weather is not defined`;
-//       break;
-//   }
-// }
-
 function displayApiResults(response) {
   console.log(response.data.main.temp);
   let temperatureElement = document.querySelector("#temperature");
@@ -218,13 +168,14 @@ function displayApiResults(response) {
   iconElement.setAttribute("src", `openweathermap/${iconID}.svg`);
 
   // let dateElement = document.querySelector("#dateTime");
-  // dateElement.innerHTML = formatDate(response.data.dt * 1000); - мне не нравится этот способ потому что
-  // время обновляется некорректно, постояннор с
-  // задержкой 10 минут и 4 дня. Наверное это из-за частоты обновления dt в API
+  // dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  // - I don't like this way because
+  // time is not updated correctly, constantly
+  // a delay of 10 minutes. This is probably due to the frequency of updating dt in the API
 }
 
 let apiKey = "ebfc1f6824f703866321e99d5ec95eb7";
-let cityName = "Kyiv";
+let cityName = "Sydney";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
 console.log(apiUrl);
 
